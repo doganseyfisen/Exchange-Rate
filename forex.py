2406f2f71abb5e78c472196f030b5701c6744fcc
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2023-04-17 04:12:29 +0300

    Create forex.py

---
+++ forex.py
@@ -0,0 +1,22 @@
+import requests
+
+print("Welcome...\nUSD = United States Dollar\nEUR = Euro\nTRY = Turkish Lira")
+
+first_currency = input("From: ").upper()
+second_currency = input("To: ").upper()
+amount = float(input("Amount: "))
+
+url = f"https://api.apilayer.com/fixer/convert?to={second_currency}&from=" \
+      f"{first_currency}&amount={amount}"
+
+payload = {}
+headers = {
+  "apikey": "QpbhGUxsulbXSKG79knPJTqeVmRMOw9E"
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+status_code = response.status_code
+json_result = response.json()
+
+print(f"\nResult: {json_result['result']} {second_currency}")
